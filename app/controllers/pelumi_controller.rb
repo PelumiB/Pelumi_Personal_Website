b700018a50eead9ce0ac07cbfbc1944b9a57420e
PelumiB <obamgbal@chicagobooth.edu>
2022-03-15 23:01:00 +0000

    Website up and running!

--- app/controllers/pelumi_controller.rb
+++ app/controllers/pelumi_controller.rb
@@ -2,4 +2,28 @@ class PelumiController < ApplicationController
   def index
     render({ :template => "pelumi/index.html" })
   end
+
+  def personal
+    render({ :template => "pelumi/personal.html" })
+  end
+
+  def education
+    render({ :template => "pelumi/education.html" })
+  end
+
+  def professional
+    render({ :template => "pelumi/professional.html" })
+  end
+
+  def personalized
+
+      @visitor = params.fetch("user")
+      cookies.store(:most_recent_addition, @visitor)
+
+    render({ :template => "pelumi/user_personalized.html" })
+  end
+  
+  def bye
+    render({ :template => "pelumi/bye.html" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "pelumi", :action => "index" })
+  get("/personal", { :controller => "pelumi", :action => "personal" })
+  get("/education", { :controller => "pelumi", :action => "education" })
+  get("/professional", { :controller => "pelumi", :action => "professional" })
+  get("/insert_name", { :controller => "pelumi", :action => "personalized" })
+  get("/bye", { :controller => "pelumi", :action => "bye" })
+
+
 
 
 
